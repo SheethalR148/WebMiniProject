0906296a611aa6fbf7de23165e1756b5a2d49969
SheethalR148 <sheethalr148@gmail.com>
2020-12-28 15:39:12 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <title>Mini Project</title>
+    <style>
+        ul {
+            position: fixed;
+            top: 0;
+        }
+        li {
+            display: inline;
+            float: left;
+
+        }
+        a {
+            /*display: block;*/
+            padding: 10px;
+            background-color: yellow;
+        }
+    </style>
+</head>
+<body>
+<!--    <ul>-->
+    <nav>
+       <a href="#home" >Home</a>
+       <a href="#menu">Menu</a>
+    </nav>
+<!--    </ul>-->
+    <?php
+    ?>
+</body>
+</html>
+
