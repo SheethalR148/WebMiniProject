1ee7d58debaa6699f55aad9966868104e71f6252
SheethalR148 <sheethalr148@gmail.com>
2020-12-28 22:09:55 +0530

    background file and welcome page

--- index.php
+++ index.php
@@ -2,31 +2,72 @@
 <head>
     <title>Mini Project</title>
     <style>
-        ul {
-            position: fixed;
-            top: 0;
+        body{
+            padding: 0;
+            margin: 0;
+            background-image: url("background.jpeg");
+            background-repeat: no-repeat;
+            background-position: center;
+            background-size: cover;
         }
-        li {
-            display: inline;
-            float: left;
+        nav{
+            color: white;
+            background-color: black;
+            height: 9%;
+            width: 100%;
+            text-align: left;
+            font-family: Cambria;
+            font-size: 40px;
+            position: fixed;
+        }
+        ul{
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
 
         }
-        a {
-            /*display: block;*/
-            padding: 10px;
-            background-color: yellow;
+        li{
+            display: inline;
+
+        }
+        li a{
+            text-decoration: none;
+            float: right;
+            padding: 5px 15px;
+            font-size: 30px;
+        }
+        div {
+            padding: 50px;
+            margin-top: 100px;
+            text-align: center;
+            font-size: 40px;
+            font-family: Algerian;
+            ;
+        }
+        .layer{
+            background-color: rgba(256,256,256,0.4);
         }
     </style>
 </head>
 <body>
-<!--    <ul>-->
     <nav>
-       <a href="#home" >Home</a>
-       <a href="#menu">Menu</a>
+        <ul>
+            &nbsp;&nbsp;<li style="font-family: Algerian">Pizza Hot</li>
+            <li><a href="">My Cart</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Menu</a></li>
+        </ul>
     </nav>
-<!--    </ul>-->
-    <?php
-    ?>
+    <br/>
+    <br/>
+    <div class="layer">
+        <h1>Welcome To Pizza Hot</h1>
+        <h3>Always served hot...</h3>
+    </div>
+    <div style="background-color: darkblue">
+        <h5 style="font-family: Cambria;color: yellow;text-align: left">About:</h5>
+        <p>PIZZA HOT is a pizza delivery website </p>
+    </div>
 </body>
 </html>
 
