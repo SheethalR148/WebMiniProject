37311988617ceff3e5183122248aecdb8e095458
SheethalR148 <sheethalr148@gmail.com>
2021-01-18 07:38:12 +0530

    Delete items on cart
    Signed-off-by: SheethalR148 <sheethalr148@gmail.com>

--- proceed.php
+++ proceed.php
@@ -17,7 +17,16 @@
 $name=$_GET['id'];
 
 //echo "<h1>$name</h1>";
+include "sql.php";
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbase = 'webmini';
 
+$conn = mysqli_connect($host, $username, $password, $dbase);
+//$did=$_GET['id'];
+$str="delete from cart where 1";
+mysqli_query($conn,$str);
 ?>
 <br/>
 <br/>
@@ -26,6 +35,13 @@ $name=$_GET['id'];
 <div style="padding: 120px;background-color: rgba(256,256,256,0.6)">
 <h1 align="center" >Thank You for Ordering!!!</h1>
 <h2 align="center">Your Order Will Be Delivered Soon...</h2>
+<h2 align="center">Currently we are offering only Cash On Delivery</h2>
+    <h2 align="center">Kindly co-operate with us by making use of card or cash</h2>
+
+<!--    <a href="" onclick="setTimeout(function() { var ww=window.open(window.location,'_self');ww.close();-->
+<!--    },1000);">CLOSE</a>-->
 </div>
+<!--<button style=" background-color: crimson;" onclick="close()">EXIT</button>-->
+
 </body>
 </html>
